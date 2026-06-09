cf4e3193b53f85704a2c105439795316563dad79
Mahesh Ayer <mahesh.ayer@westcliff.edu>
2026-06-09 08:43:19 +0545

    10names

--- Week5/script.js
+++ Week5/script.js
@@ -1,6 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  update,
+  remove,
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +27,69 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 console.log(db);
+
+//Function to write user data to Firebase Realtime Database
+function writeUserData(userId, firstname, lastname) {
+  // Get the database instance
+  // const db = getDatabase();
+
+  // Create a reference/points to 'users/{userId}' and set the data (name and email)
+  set(ref(db, "users/" + userId), {
+    firstname: firstname,
+    lastname: lastname,
+  });
+}
+writeUserData(1, "Mahesh", "Ayer");
+writeUserData(2, "A", "Ayer");
+writeUserData(3, "B", "Ayer");
+writeUserData(4, "C", "Ayer");
+writeUserData(5, "D", "Ayer");
+writeUserData(6, "E", "Ayer");
+writeUserData(7, "F", "Ayer");
+writeUserData(8, "G", "Ayer");
+writeUserData(9, "H", "Ayer");
+writeUserData(10, "K", "Ayer");
+
+// ref(db, 'users') points to the users path.
+// get(userRef) gets the data at that path.
+// snapshot.forEach(...) loops over each child node (each user).
+// childsnapshot.val() gives the actual data (name and email), which is printed.
+// function readUser() {
+//   const userRef = ref(db, "users");
+
+//   get(userRef).then((snapshot) => {
+//     snapshot.forEach((childsnapshot) => {
+//       console.log(childsnapshot.val());
+//     });
+//   });
+// }
+// readUser();
+
+// function updateUserData(userId, updatedData) {
+//   const userRef = ref(db, "users/" + userId);
+//   update(userRef, updatedData)
+//     .then(() => {
+//       console.log("User updated successfully");
+//     })
+//     .catch((error) => {
+//       console.error("Error updating user:", error);
+//     });
+// }
+// // Example usage:
+// updateUserData(2, { firstname: "Mesh", lastname: "Ayer" });
+
+// function deleteUserData(userId) {
+//   const userRef = ref(db, "users/" + userId);
+//   remove(userRef)
+//     .then(() => {
+//       console.log("User deleted successfully");
+//     })
+//     .catch((error) => {
+//       console.error("Error deleting user:", error);
+//     });
+// }
+
+// // // // // // // // // Example usage:
+// deleteUserData(1);
+
+// console.log("Added! Good");
